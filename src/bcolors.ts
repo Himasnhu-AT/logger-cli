82d540bead962985d149ac7e0a02b54d7b78c120
Himanshu-AT <aristech.official@gmail.com>
2024-07-29 17:23:08 +0530

    first commit

---
+++ src/bcolors.ts
@@ -0,0 +1,11 @@
+class bcolors {
+  static HEADER = "\x1b[95m";
+  static OKBLUE = "\x1b[94m";
+  static OKCYAN = "\x1b[96m";
+  static OKGREEN = "\x1b[92m";
+  static WARNING = "\x1b[93m";
+  static FAIL = "\x1b[91m";
+  static ENDC = "\x1b[0m";
+  static BOLD = "\x1b[1m";
+  static UNDERLINE = "\x1b[4m";
+}
